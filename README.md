042c9894e415ff2a2315878e068a7c1630844c2a
Badmingrams <161575566+Badmingrams@users.noreply.github.com>
2024-05-23 09:50:37 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 仓库说明
+
+本仓库存放csv格式的学园偶像大师翻译文本。翻译前文件可在 https://github.com/imas-tools/gakuen-adapted-stories/tree/main 找到。
+
+## 如何提交 PR
+
+### 新翻译文件
+
+将翻译后文件放于 ./tmp 中，提交PR。之后`tmp`中的文件会被自动移动到data文件夹的合适位置。
+
+### 更新翻译文件
+
+直接修改 `data/` 中的翻译文件，不要更改文件名或位置
---
+++ move.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+
+# supposed to return ***.txt
+def read_original_name(root_dir, file_path):
+    with open(os.path.join(root_dir, file_path), 'r') as file:
+        for line in file:
+            if line.startswith('info'):
+                return line.split(',')[1].strip()
+    raise ValueError("Original name not found")
+
+def move_csv_files(root_dir, dest_dir=None):
+    print("moving")
+    print("root dir:", root_dir)
+    print("dest dir:", dest_dir)
+    if dest_dir is None:
+        dest_dir = root_dir
+    # 遍历根目录及其子目录下的所有文件
+    for subdir, _, files in os.walk(root_dir):
+        for file in files:
+            print("file found: ", file)
+            if file.endswith(".csv"):
+                # 去掉文件扩展名
+                file_name = os.path.splitext(read_original_name(root_dir, file))[0]
+                # 使用下划线分隔文件名，创建新目录结构
+                new_path = os.path.join(dest_dir, *file_name.split('_')) + ".csv"
+                print(new_path)
+                new_dir = os.path.dirname(new_path)
+                # 创建新目录
+                os.makedirs(new_dir, exist_ok=True)
+                # 移动文件
+                shutil.move(os.path.join(subdir, file), new_path)
+
+# 示例调用
+root_directory = "./tmp"
+dest_directory = "./data"
+move_csv_files(root_directory, dest_directory)
